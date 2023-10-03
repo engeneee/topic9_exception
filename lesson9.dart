8bbfe71b6b62c14ff6c74b3058ffca9cc23bb21b
engeneee <zhuldyzai.rashid@jihc.kz>
2023-10-03 10:09:02 +0600

    first commit

---
+++ lesson9.dart
@@ -0,0 +1,55 @@
+//Task1
+// void main() {
+//   try {
+//     int age = 17;
+//     checkAge(age);
+//     print("Age checked");
+//   } catch (e) {
+//     print("error $e");
+//   }
+// }
+// void checkAge(int age) {
+//   if (age <= 18) {
+//     print("You are a teenager yet");
+//   }else{
+//     print("You are a young");
+//   }
+//  }
+
+
+//Task 2
+// void main() {
+//   try {
+//     int phone_number = 87029242694;
+//     number_check(phone_number);
+//   } catch (e) {
+//     print("invalid phone number $e");
+//   }
+// }
+// void number_check(int number) {
+//     if (number == 87029242694) {
+//       print('Correct phone Number');
+//     } else {
+//       print('Invalid phone number');
+//     }
+  
+//   }
+
+
+//Task 3
+//   void main() {
+//   try {
+//     String mail = "gmail.com";
+//     checkemail(mail);
+//   } catch (e) {
+//     print("Invalid email $e");
+//   }
+// }
+
+// void checkemail(String mail) {
+//   if (mail == "gmail.com") {
+//     print('Correct email');
+//   } else {
+//     print('Invalid email');
+//   }
+// }
